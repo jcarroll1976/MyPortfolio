20782d45964faeca3c52e065b9edd3af5129da0b
Joshua Carroll <carrolljosh1976@gmail.com>
2022-10-12 23:18:50 -0400

    started implementing react smooth scroll in navbar component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ import {MdOutlineKeyboardArrowRight} from 'react-icons/md';
 
 function Home() {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
-      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 sm:py-6 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
           <h2 className='sm:text-7xl font-bold text-white'>I am a Full Stack JavaScript Developer.</h2>
           <p className='text-gray-500 py-4 max-w-md'>
